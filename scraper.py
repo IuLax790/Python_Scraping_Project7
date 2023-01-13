2f4f3576a49c25e8a93c872575ee57008a7f313e
Ido Koubi <66007024+IuLax790@users.noreply.github.com>
2023-01-14 00:24:46 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,10 +2,18 @@ import selenium.webdriver as webdriver
 from selenium.webdriver.common.by import By
 driver = webdriver.Chrome("C:/Program Files/Google/Chrome/chromedriver")
 driver.get("https://www.cia.gov/the-world-factbook/countries/argentina/#people-and-society")
-data = driver.find_element(By.TAG_NAME, 'div').text
-print(data)
+country1 = driver.find_element(By.TAG_NAME, 'div').text
+print(country1)
 driver.implicitly_wait(3)
-data = driver.find_element(By.TAG_NAME, 'div').screenshot('Argentina.jpeg')
+country1 = driver.find_element(By.TAG_NAME, 'div').screenshot('Argentina.jpeg')
 print(data)
 facts = driver.find_element(By.TAG_NAME,'a').screenshot('Argentina.png')
+
+driver.implicitly_wait(3)
+country2 = driver .get("https://www.cia.gov/the-world-factbook/countries/brazil/#people-and-society")
+print(country2)
+driver.implicitly_wait(3)
+country2 = driver.find_element(By.TAG_NAME, 'div').text
+print(country2)
+facts2 = driver.find_element(By.TAG_NAME,'a')
 
